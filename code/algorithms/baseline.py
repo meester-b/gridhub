cbdb62b491acefeb6993249c824d446ae13f617c
Boukje Meester <boukjemeester@gmail.com>
2021-06-08 11:14:55 +0200

    baseline algorithm toegevoegd

---
+++ code/algorithms/baseline.py
@@ -0,0 +1,48 @@
+import random
+import copy
+from code.classes import battery, house, cable, grid
+
+dist = "1"
+
+test_grid = grid.Grid(f"data/district_{dist}/district-{dist}_houses.csv", f"data/district_{dist}/district-{dist}_batteries.csv")
+
+shortest_distance = 0
+for x in range(3):
+    print(x)
+    available_houses = copy.deepcopy(test_grid.houses)
+    current_distance = 0
+    while available_houses:
+        
+        random.shuffle(available_houses)
+        connecting_house = available_houses.pop()
+        
+        x_house = int(connecting_house.x_coordinate)
+        y_house = int(connecting_house.y_coordinate)
+    
+        random_bat = random.choice(test_grid.batteries)
+
+        x_bat = int(random_bat.x_coordinate)
+        y_bat = int(random_bat.y_coordinate)
+
+        random_bat.houses.append(connecting_house)
+
+        segment_distance = abs(x_bat - x_house) + abs(y_bat - y_house)
+        current_distance += segment_distance
+
+        # print(segment_distance)
+    
+    # print(f"The current distance is {current_distance}")
+
+    if x == 0:
+        shortest_distance = current_distance
+    elif current_distance < shortest_distance:
+        shortest_distance = current_distance
+
+print(f"The shortest distance is {shortest_distance}")
+
+
+# test_grid.print_grid()
+
+#for bat in test_grid.batteries:
+    # print(bat.houses)
+    # print(len(bat.houses))
--- main.py
+++ main.py
@@ -1,42 +1,12 @@
 import random
+import copy
+from code.algorithms import baseline
 from code.classes import battery, house, cable, grid
 # from code.visualisations import visualise as vis       
         
 if __name__ == "__main__":
-    grid_name = "district 1"
+    dist = "1"
 
-    test_grid = grid.Grid("data/district_1/district-1_houses.csv", "data/district_1/district-1_batteries.csv")
-
-    # amount_bat = 0
-    # unconnected_houses = test_grid.houses
-
-    # for battery in test_grid.batteries:
-    #     amount_house = 0 
-
-    #     for x in range(0, 30):
-
-    for x in range(0,100):
-        while test_grid.houses:
-            random.shuffle(test_grid.houses)
-            connecting_house = test_grid.houses.pop()
-        
-            random_bat = random.choice(test_grid.batteries)
-            random_bat.houses.append(connecting_house)
-
-            # randint = random.randint(0, 150 - amount_house + 30 * amount_bat)
-            # connecting_house = unconnected_houses[randint]
-            # battery.houses.append(connecting_house)
-            # unconnected_houses
-            # amount_house += 1
-
-            # amount_bat += 1
-        
-
-
-            # do your stuff with p
-
-        # test_grid.print_grid()
-
-    for bat in test_grid.batteries:
-        # print(bat.houses)
-        print(len(bat.houses))
+    test_grid = grid.Grid(f"data/district_{dist}/district-{dist}_houses.csv", f"data/district_{dist}/district-{dist}_batteries.csv")
+    
+    # call algorithm
