c00f91545b2eab814aea20544933bab94020c1cc
tkeersemaker <titus.keersemaker@gmail.com>
2021-06-11 11:13:41 +0200

    update visualisations

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from code.algorithms import baseline, greedy
 from code.classes import grid
-# from code.visualisations import visualise as vis       
+from code.visualisations.grid_plot import visualise
         
 if __name__ == "__main__":
     district = "1"
 
     test_grid = grid.Grid(f"data/district_{district}/district-{district}_houses.csv", f"data/district_{district}/district-{district}_batteries.csv")
-    
+     
+    #print(test_grid)
+    print(visualise())
+
+
     ## call algorithms
     # baseline
 
@@ -15,6 +19,6 @@ if __name__ == "__main__":
 
     # greedy
     # greedy.unconstrained_greedy()
-    greedy.Greedy.constrained_greedy(100)
+    #greedy.Greedy.constrained_greedy(100)
 
     # test_grid.run()
