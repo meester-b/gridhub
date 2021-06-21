447be16e176806d52fe95d9b30d5d403a8c4127a
tkeersemaker <titus.keersemaker@gmail.com>
2021-06-21 15:14:54 +0200

    change

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ if __name__ == "__main__":
     # climber.run(1000)
 
     # --------------------------------- Visualise --------------------------------------
-    # vis.visualise(valid_grid)
+    vis.visualise(valid_grid)
